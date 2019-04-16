c5d67fb2faadb9af9b2e6e71debc4f70123ba741
Axel Velez <axelvelez@MacBook-Pro-dAxel.local>
2019-04-16 15:59:53 +0200

    Ajout fonction de saisie

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+char saisie();
 
 int main() {
-  printf("Hello World\n");
+  char rep;
+  printf("Quelle matière voulez vous reviser ? ?\n");
+  rep = saisie();
+  printf("%c\n", rep);
   return 0;
+
+}
+
+
+char saisie() {
+  char reponse;
+  do {
+    printf("Tapez M pour Maths, P pour Physique et A pour Anglais\n");
+    scanf("%c", &reponse);
+    reponse = toupper(reponse);
+  } while(reponse != 'M' && reponse != 'P' && reponse != 'A' );
+
+  return reponse;
 }
