21157e99dacb21f343fc7d252c57a35c9df9972e
SophieShirine <50867904+SophieShirine@users.noreply.github.com>
2019-05-21 14:42:12 +0200

    Add files via upload
    cc

--- projet/lectureFichier.c
+++ projet/lectureFichier.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "struct.h"
+#include <string.h>
 
-void lectureFichier(FILE * fichier){
+void lectureFichier(FILE * fichier, S_question questions[], int i){
     int c; //Char
     char array[100] = "";
 
     while ((c = getc(fichier)) != '#' && c != EOF){
             fseek(fichier, -1, SEEK_CUR);
             fgets(array,100,fichier);
-            printf("%s", array);
+          //  printf("%s", array);
         }
+
+    strcpy(questions[i].enonce,array);
 
      printf("\n\n\n");
     //Permet de retourner jusqu'au @ suivant, puis on recule de 1
--- projet/main.c
+++ projet/main.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "struct.h"
 #include "saisie.h"
 #include "lectureFichier.h"
 #include "nbQuestions.h"
+
 
 
 int main() {
@@ -12,51 +14,37 @@ int main() {
     FILE* fichier = NULL;
     fichier = fopen("./fichier.txt", "r+");
 
-    int sol; //solution des questions
-    int pts; //points attribués aux questions
+    //int sol; //solution des questions
+    //int pts; //points attribués aux questions
 
-    int i = 1;
+    int i = 0;
     long pos_cur = 0; //position du curseur
 
-    int reponse; //reponse de l'utilisateur
+    //int reponse; //reponse de l'utilisateur
+
+
 
     if (fichier != NULL){
             ////CALCUL NOMBRE DE QUESTIONS////
         int nb_question = nombreQuestions(fichier);
         printf("\nIl y a %d questions dans cet exercice \n\n", nombreQuestions(fichier));
 
-            ////AFFICHAGE DE TOUTES LES QUESTIONS////
-        do{
-                do {
+        ////ON CREER LE TABLEAU DE STRUCTURE
+         S_question questions[nb_question];
 
-                printf("Question %d/%d : \n\n", i, nb_question);
+        ////ON ENREGISTRE TOUTES LES DONNES DANS LE TABLEAU DE STRUCTURE(ICI J'AI EGALEMENT FAIS EN SORTE DE TOUT AFFICHER POUR VERIFIER MAIS ON ENLEVERA CA APRES)
+         do {
+
+                printf("Question %d/%d : \n\n", i+1, nb_question);
                 fseek(fichier, pos_cur, SEEK_SET);
-                fscanf(fichier, "@%d %d", &sol, &pts);
-                printf("solution : %d\n", sol);
-                printf("nombre de points : %d\n", pts);
-                lectureFichier(fichier);
+                fscanf(fichier, "@%i %i", &questions[i].solution, &questions[i].points);
+                printf("solution : %i\n", questions[i].solution);
+                printf("nombre de points : %i\n", questions[i].points);
+                lectureFichier(fichier,questions,i);
+                printf("%s",questions[i].enonce);
                 pos_cur = ftell(fichier);
-
-                printf("\nEntrez votre réponse, puis appuyez sur entrée :");
-                scanf("%i",&reponse);
-
-                //Message d'erreur ou de réussite
-                if(reponse!=sol)
-                    {
-                        printf("Mauvaise reponse\n");
-                    }
-                    else
-                    {
-                        printf("ok\n");
-                    }
-
-                }while(reponse!=sol);
-
                 i++;
-                //NOTE : mettre toutes les questions dans des structures, afin que l'on puisse ensuite les utiliser librement sans avoir à se soucier du curseur
-
-        } while (i < nb_question +1);
-
+                } while (i < nb_question);
 
         fclose(fichier);
     } else {
